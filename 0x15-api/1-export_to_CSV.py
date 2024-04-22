78f425270d8f8f7828743b1a342b3586b9795765
wanjocharles <wanjocharles98@gmail.com>
2024-04-22 13:26:09 -0700

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,8 +3,8 @@
 import csv
 import json
 import sys
-import urllib
 import urllib.request
+import urllib.error
 
 # The base API url for getting the employee object
 USER_API_URL = "https://jsonplaceholder.typicode.com/users/"
@@ -13,33 +13,38 @@ USER_API_URL = "https://jsonplaceholder.typicode.com/users/"
 TODO_API_URL = "https://jsonplaceholder.typicode.com/todos?userId="
 
 # Employee ID passed as an argument to the script
-emp_id: str = sys.argv[1] if len(sys.argv) > 1 else ""
+emp_id = sys.argv[1] if len(sys.argv) > 1 else ""
 
-# Get all the todos for a given employee ID
-if emp_id.isdigit():
-    try:
-        user_url = f"{USER_API_URL}{emp_id}"
-        todos_url = f"{TODO_API_URL}{emp_id}"
+if not emp_id.isdigit():
+    print("Employee ID should be a numeric value.")
+    sys.exit(1)
 
-        emp_response = urllib.request.urlopen(user_url)
-        todos_response = urllib.request.urlopen(todos_url)
+try:
+    user_url = f"{USER_API_URL}{emp_id}"
+    todos_url = f"{TODO_API_URL}{emp_id}"
 
-        emp_data = emp_response.read()
-        todos_data = todos_response.read()
+    emp_response = urllib.request.urlopen(user_url)
+    todos_response = urllib.request.urlopen(todos_url)
 
-        employee = json.loads(emp_data)
-        todos = json.loads(todos_data)
+    emp_data = emp_response.read()
+    todos_data = todos_response.read()
 
-        username = employee.get("username")
+    employee = json.loads(emp_data)
+    todos = json.loads(todos_data)
 
-        with open(f"{emp_id}.csv", "w", newline="") as csvfile:
-            writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-            for todo in todos:
-                todo_status = todo.get("completed")
-                todo_title = todo.get("title")
-                writer.writerow([emp_id, username, todo_status, todo_title])
+    username = employee.get("username")
 
-    except urllib.error.URLError as err:
-        print(f"An error occurred: {err}")
-    except json.JSONDecodeError as err:
-        print(f"Error decoding JSON: {err}")
+    with open(f"{emp_id}.csv", "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for todo in todos:
+            todo_status = todo.get("completed")
+            todo_title = todo.get("title")
+            writer.writerow([emp_id, username, todo_status, todo_title])
+
+except urllib.error.URLError as err:
+    print(f"An error occurred while fetching data from the API: {err}")
+except json.JSONDecodeError as err:
+    print(f"Error decoding JSON data: {err}")
+except Exception as err:
+    print(f"An unexpected error occurred: {err}")
+
